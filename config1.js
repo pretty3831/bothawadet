2146396c77c154c3df263c3027a7dcd59a84f195
pretty3831 <46170310+pretty3831@users.noreply.github.com>
2019-01-05 10:18:25 +0200

    Delete config1.js

--- config1.js
+++
@@ -1,5 +0,0 @@
-exports.TOKEN = 'NTI2OTU0NTgwNDQyMDg3NDI0.DxHxlg.OA3kvH95CjgLzdggFRp470RA3sQ'; // حط توكن البوت هنا
-
-exports.PREFIX = '#';//برفكس البوت هو يلي بيشغل لاغاني
-
-exports.GOOGLE_API_KEY = 'AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8';
